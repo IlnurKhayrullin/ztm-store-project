55868a90f79254caaf356bd109e390aa14c1f30c
IlnurKhayrullin <ilnursmail@gmail.com>
2020-04-30 16:50:48 +0500

    added shop page, CollectionPreview component, CollectionItem component + styles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HomePage from "./pages/homepage/HomePage";
 import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
+import Shop from "./pages/shop/Shop";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={Shop} />
       </Switch>
     </div>
   );
